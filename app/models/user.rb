967f07b56fd583406abecb90df123d0f8b5d7d14
Noah Portes Chaikin <noah@kingandpartners.com>
2013-05-29 10:40:37 -0400

    User => self

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	
 	def self.authenticated(client)
 		instagram = client.user()
-		user = User.find_or_create_by(uid: instagram[:id])
+		user = self.find_or_create_by(uid: instagram[:id])
 		user.update_attributes(token: client.access_token, username: instagram[:username], name: instagram[:full_name], bio: instagram[:bio], picture: instagram[:profile_picture], website: instagram[:website])
 		user
 	end

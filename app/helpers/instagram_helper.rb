2bc47917129ff3839e2f69829e934f1f487d3f4b
Noah Portes Chaikin <noah@kingandpartners.com>
2013-05-29 11:53:57 -0400

    shared feed view

--- app/helpers/instagram_helper.rb
+++ app/helpers/instagram_helper.rb
@@ -1,7 +1,15 @@
 module InstagramHelper
 	
-	def image_instagram(gram)
+	def instagram_image(gram)
 		raw "<img src='?' />".gsub('?', gram['images']['standard_resolution']['url'])
+	end
+	
+	def instagram_user_link(gram)
+		link_to "@" + instagram_user(gram), "http://www.instagram.com/" + instagram_user(gram)
+	end
+	
+	def instagram_user(gram)
+		gram["user"]["username"]
 	end
 	
 end

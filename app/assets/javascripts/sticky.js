58c79cc214665e80ad6927af74e419cf42344806
Noah Portes Chaikin <noah@kingandpartners.com>
2013-05-29 13:51:00 -0400

    sticky js

---
+++ app/assets/javascripts/sticky.js
@@ -0,0 +1,25 @@
+var sticky = {
+	elements: {
+		parent: 'nav'
+	},
+	init: function () {
+		for (x in sticky.elements) {
+			sticky.elements[x] = $(sticky.elements[x]);
+			sticky.elements[x].data('pos', sticky.elements[x].offset().top)
+		}
+		sticky.scroll();
+		$(window).on('scroll', sticky.scroll);
+	},
+	scroll: function () {
+		var body = $('body').scrollTop();
+		console.log(body);
+		sticky.elements.parent.each(
+			function () {
+				var parent = { height: $(this).parent().height(), offset: $(this).parent().offset().top };
+				body > parent.offset && body < parent.offset + parent.height ? $(this).css('position', 'fixed') : $(this).css('position', 'absolute');
+			}
+		)
+	}
+}
+
+$(document).on('ready', sticky.init)
